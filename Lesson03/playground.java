5569dd9873755e8e78bf076818397bd8b7bcb58d
RaulMuroc <raulrodriguezcabestany@gmail.com>
2024-03-21 10:03:37 +0100

    command-line args and parsing

---
+++ Lesson03/playground.java
@@ -0,0 +1,13 @@
+package Lesson03;
+
+public class playground {
+    public static void main(String[] args) {
+        int[] arrayOfInts = new int[6];
+        //int[] arrayOfInts = {0,1,2,3,4,5};
+        for (int i : arrayOfInts) {
+            //String output = String.format("this int -> %i", i);
+            //System.out.println(output);
+            System.out.println("this int -> " + i);
+        } ;
+    }
+}

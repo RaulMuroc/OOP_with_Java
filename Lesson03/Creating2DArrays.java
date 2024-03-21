88ee6aceb0bdd00dd1c8fc3eeb20f2ca5c948a9f
RaulMuroc <raulrodriguezcabestany@gmail.com>
2024-03-21 10:49:10 +0100

    Creating2DArrays - extension

--- Lesson03/Creating2DArrays.java
+++ Lesson03/Creating2DArrays.java
@@ -5,5 +5,7 @@ public class Creating2DArrays {
     public static void main(String[] args) {
         double[][] array2d = {{80, 70, 75}, {69, 72, 74}};
         double[][] array2d_second = new double[2][3];
+        int nums[] = new int[100];
+        int[] nums_2 = new int[5]; // preferred
     }
 }

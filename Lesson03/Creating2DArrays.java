b53406322ab64113f0f33e6852115245c47edc8d
RaulMuroc <raulrodriguezcabestany@gmail.com>
2024-03-21 10:03:45 +0100

    creating2darrays

---
+++ Lesson03/Creating2DArrays.java
@@ -0,0 +1,8 @@
+package Lesson03;
+
+public class Creating2DArrays {
+    public static void main(String[] args) {
+        double[][] array2d = {{80, 70, 75}, {69, 72, 74}};
+        double[][] array2d = new double[2][3];
+    }
+}

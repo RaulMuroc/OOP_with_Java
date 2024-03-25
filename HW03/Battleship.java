fc13fbe0d74974ffc993aaeeb36c47cdb4ef41f5
RaulMuroc <raulrodriguezcabestany@gmail.com>
2024-03-25 22:08:05 +0100

    step -by step

--- HW03/Battleship.java
+++ HW03/Battleship.java
@@ -5,69 +5,103 @@ import java.util.Scanner; // only this package is allowed
 public class Battleship {
     public static void main(String[] args) {
         System.out.println("Welcome to Battleship!");
-
-        int currPlayer = 1;
-        int numPlayers = 2;
+        int[] RowCol = new int[2];
+        
+        // player 1
+        System.out.println("PLAYER 1, ENTER YOUR SHIPS' COORDINATES.");   
+        char[][] playerOneShotsBoard = new char[5][5];   
         int numShips = 5;
         int currShip = 1;
-        char[][] positionShipsPlayer1;
-        char[][] positionShipsPlayer2;
-
-        Scanner Scanner = new Scanner(System.in);
-
-        // promot each user to enter coordinates for five ships of lentgh one.
-        while (numPlayers > 0) {
-            boolean loopForShipsFailed = false;
-            System.out.println("PLAYER " + currPlayer + ", ENTER YOUR SHIPS' COORDINATES.");
-            // int row = 0;
-            // int col = 0;
-            while (numShips > 0) { 
-
-                System.out.println("Enter ship " + currShip + " location:");
-                //boolean stillInt = true;
-                boolean stillInt = true;
-                while(stillInt) {
-                    for(int col = 0; col++; col < 3) {
-                        for(int row = 0; row++; row < 3) {
-                    if (!Scanner.hasNextInt()) {
-                        stillInt = false;
-                        loopForShipsFailed = true;
-                    } else {
-                        row = Scanner.nextInt();
-                    }
-                    if (!Scanner.hasNextInt()) {
-                        stillInt = false;
-                        loopForShipsFailed = true;
-                    } else {
-                        col = Scanner.nextInt();
-                    }
-                    positionShipsPlayer1 = 
-                }
-                if (!stillInt) { 
-                    System.out.println("Invalid coordinates. Choose different coordinates.");
-                    break;
-                }
-                // while (!Scanner.hasNextInt()) {
-                //     System.out.println("Invalid coordinates. Choose different coordinates.");
-                // }
-                // int col = Scanner.nextInt();
-
-            }
-            
-            if (loopForShipsFailed) {
-                break;
-            }
-            System.out.print("Enter a day of the week: ");
-            String day = Scanner.next();
+        while (numShips > 0) { 
+            RowCol = CalculateRowAndCol(currShip);   
+            playerOneShotsBoard[RowCol[0]][RowCol[1]] = '@';
             numShips--;
             currShip++;
-            
-            // if (loopForShipsFailed) { 
-            //     return false;
-            // }
-            // numPlayers--;
-            // currPlayer++;
         }
-        
+
+        // player 2
+        System.out.println("PLAYER 2, ENTER YOUR SHIPS' COORDINATES.");
+        char[][] playerTwoShotsBoard = new char[5][5];
+        numShips = 5;
+        currShip = 1;
+        while (numShips > 0) { 
+            RowCol = CalculateRowAndCol(currShip);   
+            playerTwoShotsBoard[RowCol[0]][RowCol[1]] = '@';
+            numShips--;
+            currShip++;
+        }
+            
+        // prints
+        printBattleShip(playerOneShotsBoard);
+        printBattleShip(playerTwoShotsBoard);
     }
+
+    public static int[] CalculateRowAndCol(int currShip) {
+        int[] rowCol = new int[2];
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter ship " + currShip + " location:");
+        while (!Scanner.hasNextInt() || Scanner.nextInt() > 4 || Scanner.nextInt() < 0) { 
+            System.out.println("Invalid coordinates. Choose different coordinates.");
+            Scanner.nextLine();
+        } 
+        System.out.println("Check 01");
+        rowCol[0] = Scanner.nextInt();
+        System.out.println("what is rowCol[0] " + rowCol[0]); 
+        System.out.println("Check 02");
+        while (!Scanner.hasNextInt() || Scanner.nextInt() > 4 || Scanner.nextInt() < 0) {  
+            System.out.println("Invalid coordinates. Choose different coordinates.");
+            Scanner.next();
+        } 
+        System.out.println("Check 03");
+        rowCol[1] = Scanner.nextInt();
+        System.out.println("Check 04");
+        return rowCol;
+    }
+
+    private static void printBattleShip(char[][] player) {
+		System.out.print("  ");
+		for (int row = -1; row < 5; row++) {
+			if (row > -1) {
+				System.out.print(row + " ");
+			}
+			for (int column = 0; column < 5; column++) {
+				if (row == -1) {
+					System.out.print(column + " ");
+				} else {
+					System.out.print(player[row][column] + " ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+        
 }
+
+/*
+ * 
+ * import java.util.Scanner;
+public class Battleship {
+	public static void main(String[] args) {
+
+	}
+
+	// Use this method to print game boards to the console.
+	private static void printBattleShip(char[][] player) {
+		System.out.print("  ");
+		for (int row = -1; row < 5; row++) {
+			if (row > -1) {
+				System.out.print(row + " ");
+			}
+			for (int column = 0; column < 5; column++) {
+				if (row == -1) {
+					System.out.print(column + " ");
+				} else {
+					System.out.print(player[row][column] + " ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
+ * 
+*/

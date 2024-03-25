a5cbdc29e2c96d0669d9569f434233dedfdc00c4
RaulMuroc <raulrodriguezcabestany@gmail.com>
2024-03-25 09:24:14 +0100

    improved loop - still on work

--- HW03/Battleship.java
+++ HW03/Battleship.java
@@ -6,74 +6,69 @@ public class Battleship {
     public static void main(String[] args) {
         System.out.println("Welcome to Battleship!");
 
-        int currPlayer = 1;
-        int numPlayers = 2;
-        int numShips = 5;
-        int currShip = 1;
-        char[][] positionShipsPlayer1;
-        char[][] positionShipsPlayer2;
-
         Scanner Scanner = new Scanner(System.in);
 
+        int numShips = 5;
+        int currShip = 1;
+        char[][] positionShipsPlayer1 = new char[5][5];
+        
         // promot each user to enter coordinates for five ships of lentgh one.
-        while (numPlayers > 0) {
-            boolean loopForShipsFailed = false;
-            System.out.println("PLAYER " + currPlayer + ", ENTER YOUR SHIPS' COORDINATES.");
-            while (numShips > 0) { 
-                 int row = 0;
-                 int col = 0;
+        System.out.println("PLAYER 1 ENTER YOUR SHIPS' COORDINATES.");
+        while (numShips > 0) { 
+                int row = 0;
+                int col = 0;
 
-                System.out.println("Enter ship " + currShip + " location:");
-                //boolean stillInt = true;
-                //boolean stillInt = true;
-                //while(stillInt) {
-                    // for(int col = 0; col++; col < 3) {
-                    //     for(int row = 0; row++; row < 3) {
-                    
-                    // if (!stillInt) { 
-                    //     System.out.println("Invalid coordinates. Choose different coordinates.");
-                    //     break;
-                    // }    
-                    if (!Scanner.hasNextInt()) {
-                        System.out.println("Invalid coordinates. Choose different coordinates.");
-                        break;
-                        //stillInt = false;
-                        //loopForShipsFailed = true;
-                    } else {
-                        row = Scanner.nextInt();
-                    }
-                    if (!Scanner.hasNextInt()) {
-                        // stillInt = false;
-                        // loopForShipsFailed = true;
-                        System.out.println("Invalid coordinates. Choose different coordinates.");
-                        break;
-                    } else {
-                        col = Scanner.nextInt();
-                    }
-                    positionShipsPlayer1[row][col] = '@';
-                //}
-                
-                // while (!Scanner.hasNextInt()) {
-                //     System.out.println("Invalid coordinates. Choose different coordinates.");
-                // }
-                // int col = Scanner.nextInt();
-
-            }
-            
-            if (loopForShipsFailed) {
-                break;
-            }
-            System.out.print("Enter a day of the week: ");
-            String day = Scanner.next();
+            System.out.println("Enter ship " + currShip + " location:");  
+                while (!Scanner.hasNextInt()) {
+                    System.out.println("Invalid coordinates. Choose different coordinates.");
+                    Scanner.next();
+                } 
+                row = Scanner.nextInt();
+                while (!Scanner.hasNextInt()) {
+                    System.out.println("Invalid coordinates. Choose different coordinates.");
+                    Scanner.next();
+                } 
+                col = Scanner.nextInt();
+                positionShipsPlayer1[row][col] = '@';
             numShips--;
             currShip++;
+        }
+        int numShips2 = 5;
+        int currShip2 = 1;
+        char[][] positionShipsPlayer2 = new char[5][5];
+        System.out.println("PLAYER 2 ENTER YOUR SHIPS' COORDINATES.");
+        while (numShips2 > 0) { 
+                int row = 0;
+                int col = 0;
+
+            System.out.println("Enter ship " + currShip2 + " location:");  
+                while (!Scanner.hasNextInt() || row > 4 || col || > 4 || row < 0 || col < 0) {
+                    System.out.println("Invalid coordinates. Choose different coordinates.");
+                    Scanner.next();
+                } 
+                    row = Scanner.nextInt();
+                
+                while (!Scanner.hasNextInt()) {
+                    System.out.println("Invalid coordinates. Choose different coordinates.");
+                    Scanner.next();
+                } 
+                    col = Scanner.nextInt();
+                positionShipsPlayer2[row][col] = '@';
+            numShips2--;
+            currShip2++;
+        }
+
+    }
+            // if (loopForShipsFailed) {
+            //     break;
+            // }
+            // System.out.print("Enter a day of the week: ");
+            // String day = Scanner.next();
             
             // if (loopForShipsFailed) { 
             //     return false;
             // }
             // numPlayers--;
             // currPlayer++;
-        }
         
-    }
 }

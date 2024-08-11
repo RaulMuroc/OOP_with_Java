77e735d2c0635f7d1d35264c9b25e4bb6df07ee1
RaulMuroc <raulrodriguezcabestany@gmail.com>
2024-08-11 09:45:38 +0200

    JAVAII - GTx CS1331xII : HW05 -> Initialization of classes/interface

---
+++ JAVAII - GTx CS1331xII/HW05/Impostor.java
@@ -0,0 +1,17 @@
+public class Impostor {
+
+    /*
+     * Impostor.java
+This file defines an interface with the name Impostor. Impostors will be able to mess with Players through sabotage and freeze tagging them.
+
+Methods
+freeze(Player p)
+Abstract method that takes in a Player object and does not return anything
+(Note: any class that implements Impostor must provide a method definition for this method)
+sabotage(Player p)
+Abstract method that takes in a Player object and does not return anything
+(Note: any class that implements Impostor must provide a method definition for this method)
+     * 
+    */
+
+}
